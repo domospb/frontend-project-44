67b569776de96043aed66e3b5bf762e3b6efb1c9
Dmitry Arty <38582027+domospb@users.noreply.github.com>
2025-02-03 11:43:54 +0300

    add third game

---
+++ src/games/brain-gcd.js
@@ -0,0 +1,22 @@
+import runGame from '../index.js';
+
+const description = 'Find the greatest common divisor of given numbers.';
+
+const getGcd = (a, b) => {
+  if (b === 0) {
+    return Math.abs(a);
+  }
+  return getGcd(b, a % b);
+};
+
+const generateRound = () => {
+  const num1 = Math.floor(Math.random() * 100) + 1;
+  const num2 = Math.floor(Math.random() * 100) + 1;
+  
+  const question = `${num1} ${num2}`;
+  const correctAnswer = String(getGcd(num1, num2));
+
+  return [question, correctAnswer];
+};
+
+export default () => runGame(description, generateRound);
