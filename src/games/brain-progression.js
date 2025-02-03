a6d3c086f4be4afe3d22b70af7f7c4f03becbced
Dmitry Arty <38582027+domospb@users.noreply.github.com>
2025-02-03 12:55:20 +0300

    add third game

---
+++ src/games/brain-progression.js
@@ -0,0 +1,28 @@
+import runGame from '../index.js';
+
+const description = 'What number is missing in the progression?';
+
+const getProgression = (start, step, length) => {
+  const progression = [];
+  for (let i = 0; i < length; i += 1) {
+    progression.push(start + step * i);
+  }
+  return progression;
+};
+
+const generateRound = () => {
+  const start = Math.floor(Math.random() * 100);
+  const step = Math.floor(Math.random() * 10) + 1;
+  const length = Math.floor(Math.random() * 6) + 5; // random length 5-10
+  const hiddenIndex = Math.floor(Math.random() * length);
+
+  const progression = getProgression(start, step, length);
+  const correctAnswer = String(progression[hiddenIndex]);
+  progression[hiddenIndex] = '..';
+  
+  const question = progression.join(' ');
+  
+  return [question, correctAnswer];
+};
+
+export default () => runGame(description, generateRound);
